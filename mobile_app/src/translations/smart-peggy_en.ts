afb44cab69002a46bfbd1db2cb016c28aff0ee23
nazarfrenk77 <nazarfrenk77@gmail.com>
2026-08-05 16:35:43 +0300

    Add translation files

---
+++ mobile_app/src/translations/smart-peggy_en.ts
@@ -0,0 +1,175 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>BluetoothPage</name>
+    <message>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>STATUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bluetooth Enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bluetooth Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scanning...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ready to connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stop Scanning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start Scanning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AVAILABLE DEVICES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tap to connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No devices found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <source>Smart Peggy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SECTION 1: BEAM CONTROL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AUTO-SPIN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SPEED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BRIGHTNESS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SECTION 2: LIGHTING ZONES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ZONE A:
+BEACON PROJECTOR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Constant</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Strobe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SOS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ZONE B:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>LANTERNS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Garden Lights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ZONE C:
+PATHWAY GUIDE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Steady</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wave</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pulse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lighthouse_BLE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>LANGUAGE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>App Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>System (default)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ukrainian</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Russian</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DEVICE CONNECTION</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
